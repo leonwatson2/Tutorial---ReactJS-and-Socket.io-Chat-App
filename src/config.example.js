d5adc2ce5e5fc36d9340a47c79f95f2915efca8d
vlw0052 <leon@vlw2.com>
2017-11-19 21:45:01 -0600

    Added Example Config

---
+++ src/config.example.js
@@ -0,0 +1,7 @@
+// Sample config file
+// Should be changed to `config.js` when development enviornment is changed.
+
+export const socketUrl = (process.env.NODE_ENV === "development") ? 
+                          "http://localhost:3231" : 
+                          "[YOUR_PRODUCTION_SERVER_URL]" 
+
