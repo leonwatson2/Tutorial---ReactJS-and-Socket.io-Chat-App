ac7d1d38224297bf19159154633a068ca0880ba9
vlw0052 <leon@vlw2.com>
2017-11-19 21:45:25 -0600

    Git ignore updated

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const socketUrl = (process.env.NODE_ENV === "development") ? 
+                            "http://localhost:3231" : 
+                            "https://intense-brushlands-71837.herokuapp.com/" 
+
