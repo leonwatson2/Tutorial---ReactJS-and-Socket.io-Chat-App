12fb5786f92d89ff5169cf8b8e49cf6fc57ca57b
vlw0052 <leon@vlw2.com>
2018-01-06 15:52:16 -0600

    End of Tutorial Video

--- src/components/chats/ChatContainer.js
+++ src/components/chats/ChatContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import SideBar from '../sidebar/SideBar'
 import { COMMUNITY_CHAT, MESSAGE_SENT, MESSAGE_RECIEVED, 
-		TYPING, PRIVATE_MESSAGE, USER_CONNECTED, USER_DISCONNECTED } from '../../Events'
+				TYPING, PRIVATE_MESSAGE, USER_CONNECTED, USER_DISCONNECTED,
+				NEW_CHAT_USER } from '../../Events'
 import ChatHeading from './ChatHeading'
 import Messages from '../messages/Messages'
 import MessageInput from '../messages/MessageInput'
-import { values } from 'lodash'
+import { values, difference, differenceBy } from 'lodash'
 
 export default class ChatContainer extends Component {
 	constructor(props) {
@@ -28,6 +29,7 @@ export default class ChatContainer extends Component {
 		socket.off(PRIVATE_MESSAGE)
 		socket.off(USER_CONNECTED)
 		socket.off(USER_DISCONNECTED)
+		socket.off(NEW_CHAT_USER)
 	}
 	
 	initSocket(socket){
@@ -40,8 +42,11 @@ export default class ChatContainer extends Component {
 			this.setState({ users: values(users) })
 		})
 		socket.on(USER_DISCONNECTED, (users)=>{
+			const removedUsers = differenceBy( this.state.users, values(users), 'id')
+			this.removeUsersFromChat(removedUsers)
 			this.setState({ users: values(users) })			
 		})
+		socket.on(NEW_CHAT_USER, this.addUserToChat)
 	}
 
 	sendOpenPrivateMessage = (reciever) => {
@@ -49,6 +54,24 @@ export default class ChatContainer extends Component {
 		const { activeChat } = this.state
 		socket.emit(PRIVATE_MESSAGE, {reciever, sender:user.name, activeChat})
 
+	}
+	addUserToChat = ({ chatId, newUser }) => {
+		const { chats } = this.state
+		const newChats = chats.map( chat => {
+			if(chat.id === chatId){
+				return Object.assign({}, chat, { users: [ ...chat.users, newUser ] })
+			}
+			return chat
+		})
+		this.setState({ chats:newChats })
+	}
+	removeUsersFromChat = removedUsers => {
+		const { chats } = this.state
+		const newChats = chats.map( chat => {
+			let newUsers = difference( chat.users, removedUsers.map( u => u.name ) )
+				return Object.assign({}, chat, { users: newUsers })
+		})
+		this.setState({ chats: newChats })
 	}
 
 	/*
--- src/server/SocketManager.js
+++ src/server/SocketManager.js
@@ -2,7 +2,7 @@ const io = require('./index.js').io
 
 const { VERIFY_USER, USER_CONNECTED, USER_DISCONNECTED, 
 		LOGOUT, COMMUNITY_CHAT, MESSAGE_RECIEVED, MESSAGE_SENT,
-		TYPING, PRIVATE_MESSAGE  } = require('../Events')
+		TYPING, PRIVATE_MESSAGE, NEW_CHAT_USER  } = require('../Events')
 
 const { createUser, createMessage, createChat } = require('../Factories')
 
@@ -82,6 +82,15 @@ module.exports = function(socket){
 				socket.to(recieverSocket).emit(PRIVATE_MESSAGE, newChat)
 				socket.emit(PRIVATE_MESSAGE, newChat)
 			}else{
+				if(!(reciever in activeChat.users)){
+					activeChat.users
+										.filter( user => user in connectedUsers)
+										.map( user => connectedUsers[user] )
+										.map( user => {
+												socket.to(user.socketId).emit(NEW_CHAT_USER, { chatId: activeChat.id, newUser: reciever })
+										} )
+										socket.emit(NEW_CHAT_USER, { chatId: activeChat.id, newUser: reciever } )
+				}
 				socket.to(recieverSocket).emit(PRIVATE_MESSAGE, activeChat)
 			}
 		}
